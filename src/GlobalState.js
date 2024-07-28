7e4baab3b0149c29f8a33ff1d94de132178473c1
Saksham Thakur <103363770+sakshamthakur007@users.noreply.github.com>
2024-07-28 20:51:26 +0530

    Update GlobalState.js

--- src/GlobalState.js
+++ src/GlobalState.js
@@ -18,7 +18,7 @@ export const DataProvider = ({children}) => {
 
     useEffect(()=>{
         const firstLogin = localStorage.getItem('firstLogin')
-        if(!firstLogin) refreshToken()
+        if(firstLogin) refreshToken()
     },[])
 
     const state = {
