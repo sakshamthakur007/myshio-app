0c72f7c2535411ed68eedc4723346c6e987551cc
Saksham Thakur <103363770+sakshamthakur007@users.noreply.github.com>
2024-07-28 21:43:04 +0530

    Update Login.js

--- src/components/mainpages/login/Login.js
+++ src/components/mainpages/login/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import {Link} from 'react-router-dom'
 import axios from 'axios'
-
+import './login.css'
 const Login = () => {
   const [user,setUser] = useState({
     email:'',
