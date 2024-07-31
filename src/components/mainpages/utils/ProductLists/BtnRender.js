021d61023ba4ba4275cff34db5dc00f0942dbf58
Saksham Thakur <103363770+sakshamthakur007@users.noreply.github.com>
2024-07-31 16:27:01 +0530

    Update BtnRender.js

--- src/components/mainpages/utils/ProductLists/BtnRender.js
+++ src/components/mainpages/utils/ProductLists/BtnRender.js
@@ -2,33 +2,28 @@ import React, { useContext } from 'react';
 import { GlobalState } from '../../../../GlobalState';
 import { Link } from 'react-router-dom';
 
-const BtnRender = ({ product }) => {
+const BtnRender = ({ _id, product }) => {
     const state = useContext(GlobalState);
     const [isAdmin] = state.userAPI.isAdmin;
     const addCart = state.userAPI.addCart;
-
-    // The function for adding to cart, expects the entire product object
-    const handleAddToCart = () => {
-        addCart(product);
-    };
 
     return (
         <div className='row_btn'>
             {isAdmin ? (
                 <>
-                    <Link id='btn_buy' to={`#!`} >
+                    <Link id='btn_buy' to={`#!`}>
                         Delete
                     </Link>
-                    <Link id='btn_view' to={`detail/${product._id}`}>
+                    <Link id='btn_view' to={`detail/${_id}`}>
                         Edit
                     </Link>
                 </>
             ) : (
                 <>
-                    <Link id='btn_buy' to={`#!`} onClick={handleAddToCart}>
+                    <Link id='btn_buy' to={`#!`} onClick={() => addCart(_id)}>
                         Buy
                     </Link>
-                    <Link id='btn_view' to={`detail/${product._id}`}>
+                    <Link id='btn_view' to={`detail/${_id}`}>
                         View
                     </Link>
                 </>
