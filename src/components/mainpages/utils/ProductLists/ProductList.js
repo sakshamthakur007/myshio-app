328adba48a725b72674343bd78734fc02ba02530
Saksham Thakur <103363770+sakshamthakur007@users.noreply.github.com>
2024-07-31 16:21:44 +0530

    Update ProductList.js

--- src/components/mainpages/utils/ProductLists/ProductList.js
+++ src/components/mainpages/utils/ProductLists/ProductList.js
@@ -21,7 +21,7 @@ const ProductList = ({ product, isAdmin }) => {
         <p>{product.description}</p>
       </div>
 
-      <BtnRender _id={product._id} />
+     <BtnRender product={product} /
     </div>
   );
 }
