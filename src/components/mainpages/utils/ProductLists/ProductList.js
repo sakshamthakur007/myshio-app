20d303ae1c903f3bee3f89d9e00a2d28f18e03ba
Saksham Thakur <103363770+sakshamthakur007@users.noreply.github.com>
2024-08-18 22:48:51 +0530

    Update ProductList.js

--- src/components/mainpages/utils/ProductLists/ProductList.js
+++ src/components/mainpages/utils/ProductLists/ProductList.js
@@ -44,28 +44,4 @@ const ProductList = ({ product, isAdmin }) => {
 
 export default ProductList;
 
-      {isAdmin && (
-        <input 
-          type='checkbox' 
-          checked={product.checked} 
-          onChange={() => handleCheckboxChange(product._id)}
-          aria-label={`Select ${product.title}`}
-        />
-      )}
-      <img src={product.images || 'default_image_url.jpg'} alt={product.title} />
-
-      <div className='product_box'>
-        <h2 title={product.title}>{product.title}</h2>
-        <span>${product.price}</span>
-        <p>{product.description}</p>
-
-        {/* Shifting the buttons down */}
-        <div className="row_btn">
-          <BtnRender product={product} />
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default ProductList;
+      
