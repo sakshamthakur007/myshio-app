1156a6f109f55ffff007368fd83663d8b5953b08
Saksham Thakur <103363770+sakshamthakur007@users.noreply.github.com>
2024-08-18 23:19:25 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from './components/headers/Header';
 import Pages from './components/mainpages/Pages';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { DataProvider } from './GlobalState';
-
+import './App.css'
 const App = () => {
   return (
     <DataProvider>
